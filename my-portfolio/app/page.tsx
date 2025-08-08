66da932e52d3d823902f14375460aba4d53d3db9
Aishwarya S <138272330+Terraspace009@users.noreply.github.com>
2025-08-08 15:09:34 +0530

    Update page.tsx

--- my-portfolio/app/page.tsx
+++ my-portfolio/app/page.tsx
@@ -1,5 +1,3 @@
-"use client";
-import React from "react";
 import ProjectCard from "@/components/ProjectCard";
 import CertificationCard from "@/components/CertificationCard";
 import Hero from "@/components/Hero";
