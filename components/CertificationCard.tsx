edf005aad502f2c8132177f05793cd5f84184e59
Aishwarya S <138272330+Terraspace009@users.noreply.github.com>
2025-08-08 19:39:00 +0530

    Update CertificationCard.tsx

--- components/CertificationCard.tsx
+++ components/CertificationCard.tsx
@@ -7,7 +7,7 @@ interface Props {
 
 const CertificationCard: React.FC<Props> = ({ title }) => {
   return (
-    <div className="p-6 border border-purple-500 text-white rounded-lg w-64 text-center font-bold text-lg">
+    <div className="p-6 border border-purple-500 text-white rounded-lg w-64 text-center font-bold text-lg bg-black">
       {title}
     </div>
   );
