6fb7f89b7f583c0aff37f5e2eba81a3144cdae72
Aishwarya S <138272330+Terraspace009@users.noreply.github.com>
2025-08-12 11:47:25 +0530

    Update tailwind.config.js

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,7 +3,7 @@ module.exports = {
   content: [
     "./app/**/*.{js,ts,jsx,tsx}",
     "./components/**/*.{js,ts,jsx,tsx}",
-    "./pages/**/*.{js,ts,jsx,tsx}",   // fine to include even if you only use /app
+    "./pages/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
     extend: {
